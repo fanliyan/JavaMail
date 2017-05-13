70e7a5576337c6fa2784f63888e22a203552f210
fanliyan <qfanliyan@163.com>
2017-05-13 20:53:37 +0800

    添加必要注释

--- JavaMail/src/com/fly/mail/test/JavaMailTest.java
+++ JavaMail/src/com/fly/mail/test/JavaMailTest.java
@@ -6,12 +6,13 @@ import com.fly.mail.sendMail.Email;
 public class JavaMailTest {
    
 	public static void main(String[] args) {
+		//设置邮件相关信息
 		MailSenderInfo mailInfo = new MailSenderInfo();
 		mailInfo.setMailServerHost("smtp.163.com");
 		mailInfo.setMailServerPort("25");
 		mailInfo.setValidate(true);
 		mailInfo.setUserName("qfanliyan@163.com");
-		mailInfo.setPassword("520fly");
+		mailInfo.setPassword("520fly");     // 您的邮箱密码,若你的邮箱开启了客户端授权密码，则此处是您的客户端授权密码
 		mailInfo.setFromAddress("qfanliyan@163.com");
 		mailInfo.setToAddress("ifanliyan@qq.com");
 		mailInfo.setSubject("这是一封测试邮件");
